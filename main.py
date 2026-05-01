7704ca8d217b5e66dbbcbe9f4a7518af2a4f3747
IrtezaIshaq <150837618+irtazahup@users.noreply.github.com>
2026-05-01 22:40:49 +0500

    added chat endpoint in main.pyy

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 import os
 import shutil
 
+from pydantic import BaseModel
+
 from ingestion import ingest_pdf
+from embedding import get_embedding
+from retrievel import retrieve_context
+from generator import generate_answer
 
 app = FastAPI()
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-
+class ChatRequest(BaseModel):
+    question: str
+    
 @app.post("/upload-pdf/")
 async def upload_pdf(file: UploadFile = File(...)):
 
@@ -31,3 +38,21 @@ async def upload_pdf(file: UploadFile = File(...)):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+    
+@app.post("/chat")
+async def chat(request: ChatRequest):
+
+    # 1️⃣ Embedding
+    vector = get_embedding(request.question)
+
+    # 2️⃣ Retrieval
+    context = retrieve_context(vector)
+
+    # 3️⃣ Generation
+    answer = generate_answer(request.question, context)
+
+    return {
+        "answer": answer,
+        "source_context": context
+    }
